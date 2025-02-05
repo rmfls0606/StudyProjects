23e093553a97bc15e3699d03dc31f969ef70db56
rmfls0606 <rmfls0606@naver.com>
2025-02-05 16:10:02 +0900

    create New Project

---
+++ study1/study1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  study1
+//
+//  Created by 이상민 on 2/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
