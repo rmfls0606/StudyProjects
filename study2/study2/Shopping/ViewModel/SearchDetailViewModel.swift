30c96f4e85d1e7b511e0e18e068b3e09bf5ce67d
rmfls0606 <rmfls0606@naver.com>
2025-02-06 16:50:42 +0900

    SearchDetailViewModel 파일 생성

---
+++ study2/study2/Shopping/ViewModel/SearchDetailViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  SearchDetailViewModel.swift
+//  study2
+//
+//  Created by 이상민 on 2/6/25.
+//
+
+import Foundation
+
+final class SearchDetailViewModel{
+    init(){
+        print("SearchDetailViewModel init")
+    }
+    
+    deinit{
+        print("SearchDetailViewModel deinit")
+    }
+}
