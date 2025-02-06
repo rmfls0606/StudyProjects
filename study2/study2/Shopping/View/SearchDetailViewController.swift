28e70e96da1b63b35a15b47da7fe5f7d1234453d
rmfls0606 <rmfls0606@naver.com>
2025-02-06 16:18:46 +0900

    MVVM패턴으로 develop 하기 위해 Observable Model 생성

---
+++ study2/study2/Shopping/View/SearchDetailViewController.swift
@@ -0,0 +1,150 @@
+//
+//  SearchDetailViewController.swift
+//  UpDownGame
+//
+//  Created by 이상민 on 1/15/25.
+//
+
+import UIKit
+import SnapKit
+import Alamofire
+
+protocol SearchItemsViewDelegate: AnyObject{
+    func didSelectSortOption(_ sortOptions: sortOptions)
+}
+
+final class SearchDetailViewController: UIViewController, SearchItemsViewDelegate {
+    func didSelectSortOption(_ sortOptions: sortOptions) {
+        self.start = 1
+        callRequest(query: self.query!, start: 1, sortOption: sortOptions)
+    }
+    var query: String?
+    
+    let numberFormatter = NumberFormatter()
+    
+    private var start = 1
+    private var isEnd = false
+    
+    private var searchList = [Item]()
+    
+    private let cliendID = Bundle.main.infoDictionary?["XNaverClientId"] as! String
+    private let cliendSecret = Bundle.main.infoDictionary?["XNaverClientSecret"] as! String
+    
+    private lazy var collectionview = SearchItemsView()
+    
+    private lazy var totalLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .systemGreen
+        label.font = UIFont.systemFont(ofSize: 16)
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        setUp()
+        if let query = self.query{
+            callRequest(query: query, start: self.start, sortOption: collectionview.sortOption)
+        }
+    }
+    
+    private func setUp(){
+        if let query = self.query{
+            self.navigationItem.title = query
+        }
+        self.navigationController?.navigationBar.tintColor = .white
+        self.view.backgroundColor = .black
+        self.view.addSubview(collectionview)
+        self.collectionview.delegate = self
+        
+        collectionview.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        collectionview.configureDelegate(delegate: self, dataSource: self, prefetchDataSource: self)
+    }
+    
+    private func callRequest(query: String, start: Int, sortOption: sortOptions){
+        
+        let url = "https://openapi.naver.com/v1/search/shop.json?"
+
+        let parameters: [String: Any] = [
+            "query": self.query == "" ? self.query! : query,
+            "start": start,
+            "sort": sortOption.rawValue,
+            "display": 30
+        ]
+        
+        let headers: HTTPHeaders = HTTPHeaders([
+            "X-Naver-Client-Id": cliendID,
+            "X-Naver-Client-Secret": cliendSecret
+        ])
+        
+        NetworkManager.shared.loadData(url: url,
+                                       method: .get,
+                                       parameters: parameters,
+                                       headers: headers) { (result: Result<ItemList, Error>) in
+            switch result{
+            case .success(let data):
+                print(data)
+                if self.start == 1{
+                    self.searchList = data.items
+                }else{
+                    self.searchList.append(contentsOf: data.items)
+                }
+                
+                self.isEnd = self.searchList.count >= data.total
+                
+                let result = data.total.formatted(.number)
+                self.collectionview.setTotalDate(text: result)
+                self.collectionview.reloadData()
+            case .failure(let error):
+                fatalError(error.localizedDescription)
+            }
+            
+        }
+    }
+}
+
+extension SearchDetailViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        self.searchList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchItemCollectionViewCell.identifier, for: indexPath) as? SearchItemCollectionViewCell else{
+            return UICollectionViewCell()
+        }
+        let data = searchList[indexPath.item]
+        cell.configureData(data: data)
+        return cell
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+//        if indexPath.item == searchList.count - 1 && !isEnd{
+//            self.start += 1
+//            callRequest(query: self.query!, start: self.start)
+//        }
+//    }
+}
+
+extension SearchDetailViewController: UICollectionViewDataSourcePrefetching{
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        
+        guard !isEnd else { return }
+
+        for item in indexPaths{
+            if searchList.count - 5 == item.item{
+                self.start += 30
+                callRequest(query: query!, start: self.start, sortOption: self.collectionview.sortOption)
+                break
+            }
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cancelPrefetchingForItemsAt indexPaths: [IndexPath]) {
+        
+    }
+}
---
+++ study2/study2/Shopping/View/SearchItemsView.swift
@@ -0,0 +1,139 @@
+//
+//  SearchItemsView.swift
+//  UpDownGame
+//
+//  Created by 이상민 on 1/16/25.
+//
+
+import UIKit
+import SnapKit
+
+enum sortOptions: String{
+    case sim = "sim"
+    case date = "date"
+    case asc = "asc"
+    case dsc = "dsc"
+}
+
+class SearchItemsView: BaseView{
+    private(set) lazy var collectionView = createCollectionView()
+    private lazy var totalLabel = UILabel()
+    private lazy var accuracyBtn = SortButtonView(text: "정확도")
+    private lazy var dateBtn = SortButtonView(text: "날짜순")
+    private lazy var hightPrBtn = SortButtonView(text: "가격높은순")
+    private lazy var lowPrBtn = SortButtonView(text: "가격낮은순")
+    private lazy var scrollView = UIScrollView(frame: .zero)
+    private lazy var stackView = UIStackView(arrangedSubviews: [accuracyBtn,dateBtn,hightPrBtn,lowPrBtn])
+    private(set) var sortOption: sortOptions = .sim
+    
+    weak var delegate: SearchItemsViewDelegate?
+    
+    func createCollectionView() -> UICollectionView{
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.register(SearchItemCollectionViewCell.self, forCellWithReuseIdentifier: SearchItemCollectionViewCell.identifier)
+        collectionView.collectionViewLayout = createCollectionViewLayout()
+        return collectionView
+    }
+    
+    func createCollectionViewLayout() -> UICollectionViewLayout{
+        let padding = 12.0
+        let spacing = 12.0
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 12
+        layout.minimumLineSpacing = spacing
+        let width = (UIScreen.main.bounds.width - (padding * 2) - (spacing)) / 2
+        layout.itemSize = CGSize(width: width, height: 260)
+        layout.scrollDirection = .vertical
+        
+        return layout
+    }
+    
+    override func configureHierarchy() {
+        addSubview(totalLabel)
+        addSubview(scrollView)
+        scrollView.addSubview(stackView)
+        addSubview(collectionView)
+    }
+    
+    override func configureLayout() {
+        self.collectionView.backgroundColor = .black
+        
+        totalLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.equalToSuperview().offset(12)
+            make.trailing.equalToSuperview().offset(-12)
+        }
+        
+        self.scrollView.snp.makeConstraints { make in
+            make.top.equalTo(totalLabel.snp.bottom).offset(12)
+            make.leading.trailing.equalToSuperview().offset(12)
+            make.height.equalTo(40)
+        }
+        
+        stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        self.collectionView.snp.makeConstraints { make in
+            make.top.equalTo(scrollView.snp.bottom).offset(12)
+            make.bottom.equalToSuperview()
+            make.leading.equalToSuperview().offset(12)
+            make.trailing.equalToSuperview().offset(-12)
+        }
+        
+    }
+    
+    override func configureView() {
+        totalLabel.textColor = .systemGreen
+        totalLabel.font = UIFont.systemFont(ofSize: 16)
+        
+        stackView.axis = .horizontal
+        stackView.spacing = 12
+        
+        accuracyBtn.isSelected = true
+        stackView.arrangedSubviews.forEach{ btnView in
+            if let button = btnView as? SortButtonView{
+                button.delegate = self
+            }
+        }
+    }
+    
+    func configureDelegate(delegate: UICollectionViewDelegate, dataSource: UICollectionViewDataSource, prefetchDataSource: UICollectionViewDataSourcePrefetching){
+        self.collectionView.delegate = delegate
+        self.collectionView.dataSource = dataSource
+        self.collectionView.prefetchDataSource = prefetchDataSource
+    }
+    
+    func reloadData(){
+        self.collectionView.reloadData()
+    }
+    
+    func setTotalDate(text: String){
+        self.totalLabel.text = "\(text)개의 검색 결과"
+    }
+}
+
+extension SearchItemsView: SortButtonDelegate{
+    func sortButtonTapped(_ sender: SortButtonView) {
+        stackView.arrangedSubviews.forEach{ view in
+            if let button = view as? SortButtonView{
+                button.isSelected = false
+            }
+        }
+        sender.isSelected = true
+        
+        switch sender {
+        case accuracyBtn:
+            self.sortOption = .sim
+        case dateBtn:
+            self.sortOption = .date
+        case hightPrBtn:
+            self.sortOption = .asc
+        default:
+            self.sortOption = .dsc
+        }
+        
+        delegate?.didSelectSortOption(self.sortOption)
+    }
+}
