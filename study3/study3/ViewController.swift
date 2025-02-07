ef402a4d6e98f3b3a49dfd8aaa22e3d10d7c8284
rmfls0606 <rmfls0606@naver.com>
2025-02-07 23:41:37 +0900

    newProjecy 파일 생성

---
+++ study3/study3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  study3
+//
+//  Created by 이상민 on 2/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
