894d6bbd358b0e16006617ffde28ee27442f69ad
rmfls0606 <rmfls0606@naver.com>
2025-02-10 17:28:41 +0900

    MVVM패턴을 사용하기 위한 Observable Model 파일 생성 및 코드 작성

---
+++ study4/study4/Model/Observable.swift
@@ -0,0 +1,30 @@
+//
+//  Observable.swift
+//  study4
+//
+//  Created by 이상민 on 2/10/25.
+//
+
+import Foundation
+
+class Observable<T>{
+    var closure: ((T) -> Void)?
+    var value: T{
+        didSet{
+            closure?(value)
+        }
+    }
+    
+    init(_ value: T){
+        self.value = value
+    }
+    
+    func bind(closure: @escaping ((T) -> Void)){
+        closure(value)
+        self.closure = closure
+    }
+    
+    func lazyBind(closure: @escaping ((T) -> Void)){
+        self.closure = closure
+    }
+}
