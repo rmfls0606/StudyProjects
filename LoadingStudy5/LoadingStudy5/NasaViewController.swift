ecf3dbc034d9f30bfa74a09baa01a938a2a9b9dd
rmfls0606 <rmfls0606@naver.com>
2025-02-11 09:15:38 +0900

    새로운 프로젝트 생성

---
+++ LoadingStudy5/LoadingStudy5/NasaViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NasaViewController.swift
+//  LoadingStudy5
+//
+//  Created by 이상민 on 2/11/25.
+//
+
+import UIKit
+
+class NasaViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+    }
+}
---
+++ LoadingStudy5/LoadingStudy5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoadingStudy5
+//
+//  Created by 이상민 on 2/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
