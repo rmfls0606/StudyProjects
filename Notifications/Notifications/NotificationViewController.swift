0675099f7780fa863469a7ac904284a6c432d462
rmfls0606 <rmfls0606@naver.com>
2025-02-13 10:08:19 +0900

    Notifications 연습을 위한 프로젝트 생성

---
+++ Notifications/Notifications/NotificationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationViewController.swift
+//  Notifications
+//
+//  Created by 이상민 on 2/13/25.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .brown
+    }
+}
