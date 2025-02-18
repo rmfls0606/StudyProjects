6e4dadff068888276afb69baa674b8b71041f4dd
rmfls0606 <rmfls0606@naver.com>
2025-02-19 03:21:50 +0900

    텍스트 필드에 값을 입력할 때 마다 반영하여 계산기 결과 라벨을 변화시켜주는 RxSwift 코드 작성

--- RxSwiftStudy/RxSwiftStudy/NumbersViewController/NumbersViewController.swift
+++ RxSwiftStudy/RxSwiftStudy/NumbersViewController/NumbersViewController.swift
@@ -129,6 +129,11 @@ class NumbersViewController: UIViewController {
     }
     
     func setBind(){
-        
+        Observable.combineLatest(number1.rx.text.orEmpty, number2.rx.text.orEmpty, number3.rx.text.orEmpty){ textValue1, textValue2, textValue3 -> Int in
+            return (Int(textValue1) ?? 0) + (Int(textValue2) ?? 0) + (Int(textValue3) ?? 0)
+        }
+        .map { $0.description }
+        .bind(to: result.rx.text)
+        .disposed(by: disposeBag)
     }
 }
