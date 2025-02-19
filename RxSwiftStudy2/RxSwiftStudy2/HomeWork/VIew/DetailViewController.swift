12e08d4da489d9ec795e4c91cd53778a528f156c
rmfls0606 <rmfls0606@naver.com>
2025-02-19 18:40:23 +0900

    DetailVIewController파일 생성

---
+++ RxSwiftStudy2/RxSwiftStudy2/HomeWork/VIew/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  RxSwiftStudy2
+//
+//  Created by 이상민 on 2/19/25.
+//
+
+import UIKit
+import SnapKit
+
+class DetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .orange
+    }
+}
