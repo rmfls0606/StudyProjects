71001088500c49191f70c01164af6539990ca479
rmfls0606 <rmfls0606@naver.com>
2025-02-19 16:11:26 +0900

    HomeworkViewModel 기본 스타일 코드 작성

--- RxSwiftStudy2/RxSwiftStudy2/HomeWork/ViewModel/HomeWorkViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  HomeWorkViewModel.swift
-//  RxSwiftStudy2
-//
-//  Created by 이상민 on 2/19/25.
-//
-
-import Foundation
---
+++ RxSwiftStudy2/RxSwiftStudy2/HomeWork/ViewModel/HomeworkViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  HomeworkViewModel.swift
+//  RxSwiftStudy2
+//
+//  Created by 이상민 on 2/19/25.
+//
+
+import Foundation
+
+final class HomeworkViewModel{
+    struct Input{
+        
+    }
+    
+    struct Output{
+        
+    }
+    
+    func transform(){
+        
+    }
+}
