e7d6f70363b31781134d31213cdaed835278a933
rmfls0606 <rmfls0606@naver.com>
2025-02-20 10:29:13 +0900

    subscribe, bind, drive 사용법, 특성, 차이점 예제

--- RxSwiftStudy3/RxSwiftStudy3/ViewController.swift
+++ RxSwiftStudy3/RxSwiftStudy3/ViewController.swift
@@ -15,9 +15,58 @@ class ViewController: UIViewController {
     let nicknameTextField = SignTextField(placeholderText: "닉네임을 입력해주세요")
     let nextButton = PointButton(title: "다음")
     
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureLayout()
+        bindButton()
+    }
+    
+    //subscribe: next, complete, error, 메인쓰레드 실행을 보장 x
+    //bind: next. 메인쓰레드 실행을 보장 x
+    //drive, 메인쓰레드 실행을 보장, 쓰레드 공유
+    //Trait - Driver, ControlPropertey, ControlEvent etc...
+    func bindButton(){
+        //MARK: - 1) subscribe
+        //        nextButton.rx.tap
+        //            .subscribe(with: self) { owner, _ in
+        //                print(#function, "클릭")
+        //            } onError: { owner, error in
+        //                print(#function, "onError")
+        //            } onCompleted: { owner in
+        //                print(#function, "onCompleted")
+        //            } onDisposed: { owner in
+        //                print(#function, "onDisposed")
+        //            }
+        //            .disposed(by: disposeBag)
+        
+        //MARK: - 2) bind
+        //        //버튼 > 서버통신(비동기) > UI업데이트(main Thread)
+        //        nextButton.rx.tap
+        //            .map{
+        //                print("observe 전",Thread.isMainThread)
+        //            }
+        ////            .observe(on: MainScheduler.instance) //이 코드 뒤의 코드는 Main Thread에서 실행하겠어. 라는 의미의 코드
+        //            .observe(on: ConcurrentDispatchQueueScheduler(qos: .default)) //이 코드 뒤의 코드는 Background Thread에서 실행하겠어. 라는 의미의 코드
+        //            .map{
+        //                print("observe 후",Thread.isMainThread)
+        //            }
+        //            .bind(with: self) { owner, _ in
+        //                print("bind 했을 때",Thread.isMainThread)
+        //                print(#function, "클릭")
+        //                //메인
+        //            }
+        //            .disposed(by: disposeBag)
+        
+        //MARK: - 3) drive
+        nextButton.rx.tap
+            .asDriver()
+            .drive(with: self){ owner, _ in
+                print("drive")
+            }
+            .disposed(by: disposeBag)
+        
     }
     
     func configureLayout() {
