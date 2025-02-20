04b8517ca8184fe4bb67ce0adf0b45a0de1a534b
rmfls0606 <rmfls0606@naver.com>
2025-02-20 10:24:05 +0900

    예제 UI 작성

---
+++ RxSwiftStudy3/Resource.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  RxSwiftStudy3
+//
+//  Created by 이상민 on 2/20/25.
+//
+
+import UIKit
+
+enum Color {
+    static let black: UIColor = .textPoint
+    static let white: UIColor = .viewPoint
+}
--- RxSwiftStudy3/RxSwiftStudy3/ViewController.swift
+++ RxSwiftStudy3/RxSwiftStudy3/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
+    
+    let nicknameTextField = SignTextField(placeholderText: "닉네임을 입력해주세요")
+    let nextButton = PointButton(title: "다음")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .white
+        configureLayout()
     }
-
-
+    
+    func configureLayout() {
+        view.backgroundColor = .white
+        view.addSubview(nicknameTextField)
+        view.addSubview(nextButton)
+        
+        nicknameTextField.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        nextButton.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(nicknameTextField.snp.bottom).offset(30)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+    }
+    
 }
 
