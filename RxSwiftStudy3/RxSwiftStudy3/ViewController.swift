90c0984bd81e6d610ec5cffc213f603c1b37f026
rmfls0606 <rmfls0606@naver.com>
2025-02-20 11:06:17 +0900

    publisSubject, behaviorSubject 사용 및 차이점 예제

--- RxSwiftStudy3/RxSwiftStudy3/ViewController.swift
+++ RxSwiftStudy3/RxSwiftStudy3/ViewController.swift
@@ -17,10 +17,64 @@ class ViewController: UIViewController {
     
     let disposeBag = DisposeBag()
     
+    
+    let publisSubject = PublishSubject<Int>() //PublishSubject: 초기값 없음
+    let behaviorSubject = BehaviorSubject(value: 0) //BehaviorSubject: 초기값 설정해줘야 함
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureLayout()
-        bindButton()
+        bindTextField()
+    }
+    
+    func bindTextField(){
+        nicknameTextField.rx.text.orEmpty
+            .subscribe(with: self) { owner, value in
+                print(#function, value)
+                print("실시간으로 텍스트필드 달라짐")
+            } onError: { owner, error in
+                print(#function, "onError")
+            } onCompleted: { owner in
+                print(#function, "onCompleted")
+            } onDisposed: { owner in
+                print(#function, "onDisposed")
+            }
+            .disposed(by: disposeBag)
+        
+        //처음에는 초기값이 없기에 출력안됨.
+        //버튼 클릭으로 값이 들어오면 출력됨
+        publisSubject
+            .subscribe(with: self) { owner, value in
+                print(#function, value)
+                print("publisSubject")
+            } onError: { owner, error in
+                print(#function, "onError")
+            } onCompleted: { owner in
+                print(#function, "onCompleted")
+            } onDisposed: { owner in
+                print(#function, "onDisposed")
+            }
+            .disposed(by: disposeBag)
+        
+        behaviorSubject
+            .subscribe(with: self) { owner, value in
+                print(#function, value)
+                print("behaviorSubject")
+            } onError: { owner, error in
+                print(#function, "onError")
+            } onCompleted: { owner in
+                print(#function, "onCompleted")
+            } onDisposed: { owner in
+                print(#function, "onDisposed")
+            }
+            .disposed(by: disposeBag)
+        
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                owner.behaviorSubject.onNext(4)
+                owner.publisSubject.onNext(3)
+            }
+            .disposed(by: disposeBag)
     }
     
     /*
@@ -71,81 +125,81 @@ class ViewController: UIViewController {
      }
      */
     
-    func bindButton(){
-        let button = nextButton.rx.tap
-            .map{ print("버튼 클릭")}
-            .debug("1")
-            .debug("2")
-            .debug("3")
-            .map{ "안녕하세요 \(Int.random(in: 1...100))"}
-        //Driver의 경우 share연산자를 내부적으로 가지고 있어서 사용하지 않아도 됨
-//            .share() //하나의 subscribe를 공유하도록
-//            .asDriver(onErrorJustReturn: "") //실패를 하거나 문자가 안 올때 해당 값을 return 해줘라
-        
-        //MARK: - 기본 bind 예제
-        
-         button
-         .bind(with: self) { owner, value in
-         print("1", value)
-         }
-         .disposed(by: disposeBag)
-         
-         button
-         .bind(with: self) { owner, value in
-         print("2", value)
-         }
-         .disposed(by: disposeBag)
-         
-         button
-         .bind(with: self) { owner, value in
-         print("3", value)
-         }
-         .disposed(by: disposeBag)
-         
-         /*
-          1 안녕하세요 13
-          2 안녕하세요 98
-          3 안녕하세요 50
-          */
-        
-        //MARK: - share 연산자를 사용한 예제
-        /*
-            button
-                .bind(to: navigationItem.rx.title)
-                .disposed(by: disposeBag)
-            
-            button
-                .bind(to: nextButton.rx.title())
-                .disposed(by: disposeBag)
-            
-            button
-                .bind(to: nicknameTextField.rx.text)
-                .disposed(by: disposeBag)
-            
-            /*
-             각 to: _ 에 해당하는 곳에 같은 값들이 존재함
-             */
-         */
-        
-        //MARK: - drive 바인딩을 사용한 예제
-        /*
-            button
-                .drive(navigationItem.rx.title)
-                .disposed(by: disposeBag)
-            
-            button
-                .drive(nextButton.rx.title())
-                .disposed(by: disposeBag)
-            
-            button
-                .drive(nicknameTextField.rx.text)
-                .disposed(by: disposeBag)
-            
-            /*
-             각 to: _ 에 해당하는 곳에 같은 값들이 존재함
-             */
-         */
-    }
+//    func bindButton(){
+//        let button = nextButton.rx.tap
+//            .map{ print("버튼 클릭")}
+//            .debug("1")
+//            .debug("2")
+//            .debug("3")
+//            .map{ "안녕하세요 \(Int.random(in: 1...100))"}
+//        //Driver의 경우 share연산자를 내부적으로 가지고 있어서 사용하지 않아도 됨
+////            .share() //하나의 subscribe를 공유하도록
+////            .asDriver(onErrorJustReturn: "") //실패를 하거나 문자가 안 올때 해당 값을 return 해줘라
+//        
+//        //MARK: - 기본 bind 예제
+//        
+//         button
+//         .bind(with: self) { owner, value in
+//         print("1", value)
+//         }
+//         .disposed(by: disposeBag)
+//         
+//         button
+//         .bind(with: self) { owner, value in
+//         print("2", value)
+//         }
+//         .disposed(by: disposeBag)
+//         
+//         button
+//         .bind(with: self) { owner, value in
+//         print("3", value)
+//         }
+//         .disposed(by: disposeBag)
+//         
+//         /*
+//          1 안녕하세요 13
+//          2 안녕하세요 98
+//          3 안녕하세요 50
+//          */
+//        
+//        //MARK: - share 연산자를 사용한 예제
+//        /*
+//            button
+//                .bind(to: navigationItem.rx.title)
+//                .disposed(by: disposeBag)
+//            
+//            button
+//                .bind(to: nextButton.rx.title())
+//                .disposed(by: disposeBag)
+//            
+//            button
+//                .bind(to: nicknameTextField.rx.text)
+//                .disposed(by: disposeBag)
+//            
+//            /*
+//             각 to: _ 에 해당하는 곳에 같은 값들이 존재함
+//             */
+//         */
+//        
+//        //MARK: - drive 바인딩을 사용한 예제
+//        /*
+//            button
+//                .drive(navigationItem.rx.title)
+//                .disposed(by: disposeBag)
+//            
+//            button
+//                .drive(nextButton.rx.title())
+//                .disposed(by: disposeBag)
+//            
+//            button
+//                .drive(nicknameTextField.rx.text)
+//                .disposed(by: disposeBag)
+//            
+//            /*
+//             각 to: _ 에 해당하는 곳에 같은 값들이 존재함
+//             */
+//         */
+//    }
     
     func configureLayout() {
         view.backgroundColor = .white
