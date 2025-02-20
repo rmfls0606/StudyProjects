2618999ed27720be19add44eca3e03c0047fe3c3
rmfls0606 <rmfls0606@naver.com>
2025-02-20 14:06:21 +0900

    새로운 HomeworkViewController 파일 추가

---
+++ RxSwiftStudy3/RxSwiftStudy3/HomeWorkView/HomeworkViewController.swift
@@ -0,0 +1,70 @@
+//
+//  HomeworkViewController.swift
+//  RxSwift
+//
+//  Created by Jack on 1/30/25.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+struct Person: Identifiable {
+    let id = UUID()
+    let name: String
+    let email: String
+    let profileImage: String
+}
+
+class HomeworkViewController: UIViewController {
+    
+    let tableView = UITableView()
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout())
+    let searchBar = UISearchBar()
+     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        bind()
+    }
+     
+    private func bind() {
+          
+    }
+    
+    private func configure() {
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+        view.addSubview(collectionView)
+        view.addSubview(searchBar)
+        
+        navigationItem.titleView = searchBar
+         
+        collectionView.register(UserCollectionViewCell.self, forCellWithReuseIdentifier: UserCollectionViewCell.identifier)
+        collectionView.backgroundColor = .lightGray
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalToSuperview()
+            make.height.equalTo(50)
+        }
+        
+        tableView.register(PersonTableViewCell.self, forCellReuseIdentifier: PersonTableViewCell.identifier)
+        tableView.backgroundColor = .systemGreen
+        tableView.rowHeight = 100
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(50)
+            make.horizontalEdges.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+    }
+    
+    private func layout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 80, height: 40)
+        layout.scrollDirection = .horizontal
+        return layout
+    }
+
+}
+ 
