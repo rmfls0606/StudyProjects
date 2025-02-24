61bb2d685bf19065d1f6782c6a29a9529ed13bbe
rmfls0606 <rmfls0606@naver.com>
2025-02-24 15:03:49 +0900

    코드는 잘 작성하였지만 성공했을 때도 에러를 발생시켜 순서 확인해보기

--- RxSwiftStudy3/RxSwiftStudy3/NewSearchView/NetworkManager.swift
+++ RxSwiftStudy3/RxSwiftStudy3/NewSearchView/NetworkManager.swift
@@ -23,59 +23,14 @@ final class NetworkManager{
     static let shared = NetworkManager()
     
     private init() { }
-//    func callBoxOffice(date: String) -> Observable<Movie>{
-//        
-//        return Observable<Movie>.create { value in
-//            
-//            let url =  "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=1e11e012e5de56d8598e901746dc0848&targetDt=\(date)"
-//            
-//            guard let url = URL(string: url) else {
-//                value.onError(APIError.invalidURL)
-//                return Disposables.create {
-//                    print("끝")
-//                }
-//            }
-//            
-//            URLSession.shared.dataTask(with: url) { data, response, error in
-//                if let error = error{
-//                    value.onError(APIError.unknownResponse)
-//                    return
-//                }
-//                
-//                guard let response = response as? HTTPURLResponse,
-//                      (200...299).contains(response.statusCode) else{
-//                    value.onError(APIError.statusError)
-//                    return
-//                }
-//                
-//                if let data = data{
-//                    do{
-//                        let result = try JSONDecoder().decode(Movie.self, from: data)
-//                        value.onNext(result)
-//                        value.onCompleted()//원하는 데이터 전달 시 종료 필요!!(중요!!!)
-//                    }catch{
-//                        value.onError(APIError.unknownResponse)
-//                    }
-//                }else{
-//                    value.onError(APIError.unknownResponse)
-//                }
-//            }
-//            .resume()
-//            
-//            return Disposables.create {
-//                print("끝")
-//            }
-//        }
-//    }
-    func callBoxOfficeWithSingle(date: String) -> Single<Movie>{
-        //single:  next가 끝나면 자동으로 completed해주는 기능
-
-        return Single<Movie>.create { value in
+    func callBoxOffice(date: String) -> Observable<Movie>{
+        
+        return Observable<Movie>.create { value in
             
             let url =  "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=1e11e012e5de56d8598e901746dc0848&targetDt=\(date)"
             
             guard let url = URL(string: url) else {
-                value(.failure(APIError.invalidURL))
+                value.onError(APIError.invalidURL)
                 return Disposables.create {
                     print("끝")
                 }
@@ -83,25 +38,28 @@ final class NetworkManager{
             
             URLSession.shared.dataTask(with: url) { data, response, error in
                 if let error = error{
-                    value(.failure(APIError.unknownResponse))
+                    value.onError(APIError.unknownResponse)
                     return
                 }
                 
                 guard let response = response as? HTTPURLResponse,
                       (200...299).contains(response.statusCode) else{
-                    value(.failure(APIError.statusError))
+                    value.onError(APIError.statusError)
                     return
                 }
                 
                 if let data = data{
                     do{
                         let result = try JSONDecoder().decode(Movie.self, from: data)
-                        value(.success(result))
+                        value.onError(APIError.unknownResponse)
+                        
+//                        value.onNext(result)
+//                        value.onCompleted()//원하는 데이터 전달 시 종료 필요!!(중요!!!)
                     }catch{
-                        value(.failure(APIError.unknownResponse))
+                        value.onError(APIError.unknownResponse)
                     }
                 }else{
-                    value(.failure(APIError.unknownResponse))
+                    value.onError(APIError.unknownResponse)
                 }
             }
             .resume()
@@ -111,4 +69,48 @@ final class NetworkManager{
             }
         }
     }
+//    func callBoxOfficeWithSingle(date: String) -> Single<Movie>{
+//        //single:  next가 끝나면 자동으로 completed해주는 기능
+//
+//        return Single<Movie>.create { value in
+//            
+//            let url =  "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=1e11e012e5de56d8598e901746dc0848&targetDt=\(date)"
+//            
+//            guard let url = URL(string: url) else {
+//                value(.failure(APIError.invalidURL))
+//                return Disposables.create {
+//                    print("끝")
+//                }
+//            }
+//            
+//            URLSession.shared.dataTask(with: url) { data, response, error in
+//                if let error = error{
+//                    value(.failure(APIError.unknownResponse))
+//                    return
+//                }
+//                
+//                guard let response = response as? HTTPURLResponse,
+//                      (200...299).contains(response.statusCode) else{
+//                    value(.failure(APIError.statusError))
+//                    return
+//                }
+//                
+//                if let data = data{
+//                    do{
+//                        let result = try JSONDecoder().decode(Movie.self, from: data)
+//                        value(.success(result))
+//                    }catch{
+//                        value(.failure(APIError.unknownResponse))
+//                    }
+//                }else{
+//                    value(.failure(APIError.unknownResponse))
+//                }
+//            }
+//            .resume()
+//            
+//            return Disposables.create {
+//                print("끝")
+//            }
+//        }
+//    }
 }
