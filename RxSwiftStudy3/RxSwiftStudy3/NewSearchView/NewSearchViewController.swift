88bebadfae50670a4bf05cd6f165c08e85f46387
rmfls0606 <rmfls0606@naver.com>
2025-02-24 10:08:47 +0900

    셀 선택 시 문자열로 받게 하는 기본적인 코드

---
+++ RxSwiftStudy3/RxSwiftStudy3/NewSearchView/NewSearchViewController.swift
@@ -0,0 +1,77 @@
+//
+//  NewSearchViewController.swift
+//  RxSwiftStudy3
+//
+//  Created by 이상민 on 2/24/25.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+
+class NewSearchViewController: UIViewController {
+    let disposeBag = DisposeBag()
+    
+    let viewModel = NewSearchViewModel()
+    
+    private let tableView: UITableView = {
+        let view = UITableView()
+        view.register(SearchTableViewCell.self, forCellReuseIdentifier: SearchTableViewCell.identifier)
+        view.backgroundColor = .lightGray
+        view.rowHeight = 180
+        view.separatorStyle = .none
+        return view
+    }()
+    
+    let searchBar = UISearchBar()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setSearchController()
+        configure()
+        bind()
+    }
+    
+    func bind(){
+        //MARK: - Input
+        let input = NewSearchViewModel.Input(searchTap: searchBar.rx.searchButtonClicked, searchText: searchBar.rx.text.orEmpty)
+        
+        //MARK: - Output
+        let output = viewModel.transform(input: input)
+        output.list
+            .bind(
+                to: tableView.rx
+                    .items(
+                        cellIdentifier: SearchTableViewCell.identifier,
+                        cellType: SearchTableViewCell.self
+                    )
+            ){(row, element, cell) in
+                cell.appNameLabel.text = element
+            }
+            .disposed(by: disposeBag)
+        
+        tableView.rx.itemSelected
+            .map{
+                "\($0) 텍스트"
+            }
+            .subscribe(with: self) { owner, value in
+                print(value)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    private func setSearchController() {
+        view.addSubview(searchBar)
+        navigationItem.titleView = searchBar
+    }
+    
+    private func configure() {
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+    }
+}
---
+++ RxSwiftStudy3/RxSwiftStudy3/NewSearchView/NewSearchViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  NewSearchViewModel.swift
+//  RxSwiftStudy3
+//
+//  Created by 이상민 on 2/24/25.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class NewSearchViewModel{
+    
+    let disposeBag = DisposeBag()
+
+    struct Input{
+        let searchTap: ControlEvent<Void>
+        let searchText: ControlProperty<String>
+    }
+    
+    struct Output{
+        let list: Observable<[String]>
+    }
+    
+    func transform(input: Input) -> Output{
+        let list = Observable.just(["기"])
+        
+        //map, withLatestFrom, flatmap, flatMapLatest etc...
+        input.searchTap
+            .withLatestFrom(input.searchText)
+            .subscribe(
+                with: self) { owner, value in
+                    print("next", value)
+                } onError: { owner, error in
+                    print("onError")
+                } onCompleted: { owner in
+                    print("onCompleted")
+                } onDisposed: { owner in
+                    print("onDisposed")
+                }
+                .disposed(by: disposeBag)
+        
+        return Output(list: list)
+    }
+}
