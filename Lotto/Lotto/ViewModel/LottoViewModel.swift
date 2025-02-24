2bf303b07d19bc5cbaf8cd2b69a9b1837f371cde
rmfls0606 <rmfls0606@naver.com>
2025-02-24 16:22:47 +0900

    뷰모델 파일 생성

---
+++ Lotto/Lotto/ViewModel/LottoViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  LottoViewModel.swift
+//  Lotto
+//
+//  Created by 이상민 on 2/24/25.
+//
+
+import Foundation
+
+class LottoViewModel{
+    
+    struct Input{
+        
+    }
+    
+    struct Output{
+        
+    }
+    
+    func transform(input: Input) -> Output{
+        return Output()
+    }
+}
