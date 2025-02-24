3ef4ff7415f0cd5184de1fb3a71245dbe8cc25da
rmfls0606 <rmfls0606@naver.com>
2025-02-24 16:00:45 +0900

    로또 프로젝트 생성, 스토리보드 삭제, SceneDelegate로 연결

---
+++ Lotto/Lotto/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Lotto
+//
+//  Created by 이상민 on 2/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+}
+
