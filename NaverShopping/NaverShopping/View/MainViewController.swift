d0c40972359d23c6365be105a074a7864f4a90b0
rmfls0606 <rmfls0606@naver.com>
2025-02-25 19:08:27 +0900

    레이아웃을 잡기 위해 SnapKit 패키지 추가 및  서치바 UI 추가

--- NaverShopping/NaverShopping/View/MainViewController.swift
+++ NaverShopping/NaverShopping/View/MainViewController.swift
@@ -6,17 +6,34 @@
 //
 
 import UIKit
+import SnapKit
 
-class MainViewController: BaseViewController {
+final class MainViewController: BaseViewController {
+    
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "브랜드, 상품, 프로필, 태그 등"
+        searchBar.barTintColor = .black
+        searchBar.searchTextField.backgroundColor = UIColor(named: "searchBarbc")
+        searchBar.searchTextField.attributedPlaceholder = NSAttributedString(string: searchBar.placeholder!, attributes: [.foregroundColor: UIColor.lightGray])
+        searchBar.searchTextField.textColor = .lightGray
+        searchBar.searchTextField.leftView?.tintColor = .lightGray
+        return searchBar
+    }()
+    
     override func configureHierarchy() {
-        
+        self.view.addSubview(searchBar)
     }
     
     override func configureLayout() {
-        
+        self.searchBar.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide)
+            make.leading.trailing.equalToSuperview()
+        }
     }
     
     override func configureView() {
         self.view.backgroundColor = .black
+        self.navigationItem.title = "도봉러의 쇼핑쇼핑"
     }
 }
