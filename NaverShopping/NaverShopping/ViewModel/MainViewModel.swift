4c46aa770f7470a109cb7b80c89fc6b19194b1ea
rmfls0606 <rmfls0606@naver.com>
2025-02-25 20:19:19 +0900

    Input/Output 구조의 ViewModel 파일 생성

---
+++ NaverShopping/NaverShopping/ViewModel/MainViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewModel.swift
+//  NaverShopping
+//
+//  Created by 이상민 on 2/25/25.
+//
+
+import Foundation
+
+final class MainViewModel{
+    struct Input{
+        
+    }
+    
+    struct Output{
+        
+    }
+    
+    func transform(input: Input) -> Output{
+        return Output()
+    }
+}
