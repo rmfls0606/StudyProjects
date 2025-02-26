824569eedd34c9cab0c5e7ef199fce103a93b149
rmfls0606 <rmfls0606@naver.com>
2025-02-26 18:13:13 +0900

    그룹으로 나오는 리스트형식의 collectionView 완성

--- NaverShopping/NaverShopping/View/WishListViewController.swift
+++ NaverShopping/NaverShopping/View/WishListViewController.swift
@@ -8,7 +8,18 @@
 import UIKit
 import SnapKit
 
+struct Wish: Hashable{
+    let name: String
+    let date = Date()
+}
+
 class WishListViewController: BaseViewController {
+    private var dateFormatter: DateFormatter{
+        let dateFormatted = DateFormatter()
+        dateFormatted.dateFormat = "yyyy년 MM월 dd일"
+        return dateFormatted
+    }
+    
     private let searchBar: UISearchBar = {
         let searchBar = UISearchBar()
         searchBar.placeholder = "위시리스트"
@@ -20,10 +31,15 @@ class WishListViewController: BaseViewController {
         return searchBar
     }()
     
-    //collectionView.register을 대신하는 코드
-    private var registration: UICollectionView.CellRegistration<UICollectionViewListCell, String>!
+    //numberOfItemsInSection, cellforItemAt을 대신해준다.
+    var dataSource: UICollectionViewDiffableDataSource<String, Wish>!
     
-    var list = ["킁킁", "냄새", "향기"] //임시 데이터
+//    var list = ["킁킁", "냄새", "향기"] //임시 데이터
+    var list = [
+        Wish(name: "킁킁"),
+        Wish(name: "냄세"),
+        Wish(name: "형기")
+    ]
     
     lazy var collectionView = UICollectionView(
         frame: .zero,
@@ -35,6 +51,7 @@ class WishListViewController: BaseViewController {
             appearance: .plain
         )
         
+        configuraion.backgroundColor = .black
         let layout = UICollectionViewCompositionalLayout.list(
             using: configuraion
         )
@@ -63,20 +80,36 @@ class WishListViewController: BaseViewController {
         self.title = "위시리스트"
         
         self.collectionView.delegate = self
-        self.collectionView.dataSource = self
         
-        configureCell()
+//        configureCell()
+        configureDataSource()
+        updateSnapshot()
     }
     
-    private func configureCell(){
+    private func updateSnapshot(){
+        var snapshot = NSDiffableDataSourceSnapshot<String, Wish>()
+        snapshot.appendSections(["위시리스트"])
+        snapshot.appendItems(list, toSection: "위시리스트")
+        
+        dataSource.apply(snapshot)
+    }
+    
+    private func configureDataSource(){
+        //collectionView.register을 대신하는 코드
+        var registration: UICollectionView.CellRegistration<UICollectionViewListCell, Wish>!
+        
         //cellForItemAt 내부 코드
         registration = UICollectionView.CellRegistration(handler: { cell, indexPath, itemIdentifier in
             
             var content = UIListContentConfiguration.valueCell()
             
-            content.text = itemIdentifier
+            content.text = itemIdentifier.name
             content.textProperties.color = .brown
             content.textProperties.font = .boldSystemFont(ofSize: 20)
+            
+            content.secondaryText = self.dateFormatter
+                .string(from: itemIdentifier.date)
+            content.secondaryTextProperties.color = .gray
             
             cell.contentConfiguration = content
             
@@ -88,26 +121,63 @@ class WishListViewController: BaseViewController {
             
             cell.backgroundConfiguration = backgroundConfig
         })
+        
+        dataSource = UICollectionViewDiffableDataSource(
+            collectionView: collectionView,
+            cellProvider: {
+ collectionView,
+                indexPath,
+                itemIdentifier in
+                let cell = collectionView.dequeueConfiguredReusableCell(
+                    using: registration,
+                    for: indexPath,
+                    item: itemIdentifier
+                )
+                return cell
+            }
+        )
     }
+    
+//    private func configureCell(){
+//        //cellForItemAt 내부 코드
+//        registration = UICollectionView.CellRegistration(handler: { cell, indexPath, itemIdentifier in
+//            
+//            var content = UIListContentConfiguration.valueCell()
+//            
+//            content.text = itemIdentifier.name
+//            content.textProperties.color = .brown
+//            content.textProperties.font = .boldSystemFont(ofSize: 20)
+//            
+//            cell.contentConfiguration = content
+//            
+//            var backgroundConfig = UIBackgroundConfiguration.listCell()
+//            backgroundConfig.backgroundColor = .yellow
+//            backgroundConfig.cornerRadius = 10
+//            backgroundConfig.strokeWidth = 2.0
+//            backgroundConfig.strokeColor = .red
+//            
+//            cell.backgroundConfiguration = backgroundConfig
+//        })
+//    }
 }
 
 //MARK: - CollectionView
-extension WishListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        list.count
-    }
+extension WishListViewController: UICollectionViewDelegate {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        list.count
+//    }
 
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueConfiguredReusableCell(
-            using: registration,
-            for: indexPath,
-            item: list[indexPath.item]
-        )
-        
-        cell.backgroundColor = .orange
-        
-        return cell
-    }
-
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueConfiguredReusableCell(
+//            using: registration,
+//            for: indexPath,
+//            item: list[indexPath.item]
+//        )
+//        
+//        cell.backgroundColor = .orange
+//        
+//        return cell
+//    }
+//
     
 }
