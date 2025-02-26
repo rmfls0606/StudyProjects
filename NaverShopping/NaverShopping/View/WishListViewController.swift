4bbaa931f80d8cc5bcced750648c75c47d6cc3d5
rmfls0606 <rmfls0606@naver.com>
2025-02-26 17:15:57 +0900

    더미데이터 리스트 형식의 collectionView로 나오게 하기

--- NaverShopping/NaverShopping/View/WishListViewController.swift
+++ NaverShopping/NaverShopping/View/WishListViewController.swift
@@ -20,6 +20,11 @@ class WishListViewController: BaseViewController {
         return searchBar
     }()
     
+    //collectionView.register을 대신하는 코드
+    private var registration: UICollectionView.CellRegistration<UICollectionViewListCell, String>!
+    
+    var list = ["킁킁", "냄새", "향기"] //임시 데이터
+    
     lazy var collectionView = UICollectionView(
         frame: .zero,
         collectionViewLayout: createLayout()
@@ -39,16 +44,70 @@ class WishListViewController: BaseViewController {
     
     override func configureHierarchy() {
         self.view.addSubview(searchBar)
+        self.view.addSubview(collectionView)
     }
     
     override func configureLayout() {
         self.searchBar.snp.makeConstraints { make in
             make.top.leading.trailing.equalTo(self.view.safeAreaLayoutGuide)
         }
+        
+        self.collectionView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom).offset(12)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
     }
     
     override func configureView() {
         self.view.backgroundColor = .black
         self.title = "위시리스트"
+        
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        
+        configureCell()
     }
+    
+    private func configureCell(){
+        //cellForItemAt 내부 코드
+        registration = UICollectionView.CellRegistration(handler: { cell, indexPath, itemIdentifier in
+            
+            var content = UIListContentConfiguration.valueCell()
+            
+            content.text = itemIdentifier
+            content.textProperties.color = .brown
+            content.textProperties.font = .boldSystemFont(ofSize: 20)
+            
+            cell.contentConfiguration = content
+            
+            var backgroundConfig = UIBackgroundConfiguration.listCell()
+            backgroundConfig.backgroundColor = .yellow
+            backgroundConfig.cornerRadius = 10
+            backgroundConfig.strokeWidth = 2.0
+            backgroundConfig.strokeColor = .red
+            
+            cell.backgroundConfiguration = backgroundConfig
+        })
+    }
+}
+
+//MARK: - CollectionView
+extension WishListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        list.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueConfiguredReusableCell(
+            using: registration,
+            for: indexPath,
+            item: list[indexPath.item]
+        )
+        
+        cell.backgroundColor = .orange
+        
+        return cell
+    }
+
+    
 }
