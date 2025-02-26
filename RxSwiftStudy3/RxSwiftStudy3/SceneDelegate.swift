9cd06d2136be52c00e8776085913b43ebf5152fa
rmfls0606 <rmfls0606@naver.com>
2025-02-26 11:47:30 +0900

    사용자 모델을 정의 하여 컬렉션 뷰 만들기

--- RxSwiftStudy3/RxSwiftStudy3/SceneDelegate.swift
+++ RxSwiftStudy3/RxSwiftStudy3/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = UINavigationController(
-            rootViewController: SimpleTableViewController()
+            rootViewController: SimpleCollectionViewController()
         )
         window?.makeKeyAndVisible()
     }
--- RxSwiftStudy3/RxSwiftStudy3/SimpleTableView/SimpleCollectionViewController.swift
+++ RxSwiftStudy3/RxSwiftStudy3/SimpleTableView/SimpleCollectionViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 /*
  Data
  -> Delegate, DataSource (가장 큰 특징: 인덱스 기반)
@@ -21,6 +22,14 @@ import UIKit
  ->
  -> List Cell
  */
+
+struct Product {
+    let name: String
+    let price = Int.random(in: 1...10000) * 1000
+    let count = Int.random(in: 1...10)
+}
+
+
 class SimpleCollectionViewController: UIViewController {
 
     //Flow -> compositional -> List configuration
@@ -29,12 +38,31 @@ class SimpleCollectionViewController: UIViewController {
         collectionViewLayout: createLayout()
     )
     
+    //collectionView.register 대신
+    var registration: UICollectionView.CellRegistration<UICollectionViewListCell, Product>!
+    
+//    var list = [
+//        "Hue", "Jack", "Bran", "Den"
+//    ]
+//    var list = [
+//        1, 234, 555, 9006, 618
+//    ]
+    
+    var list = [
+        Product(name: "Macbook Pro M5"),
+        Product(name: "키보드"),
+        Product(name: "트랙패드"),
+        Product(name: "금")
+    ]
+    
     //테이블 뷰 시스템 기능을 컬렉션뷰로도 만들 수 있다.
     func createLayout() -> UICollectionViewLayout{
         
         var configuration = UICollectionLayoutListConfiguration(
             appearance: .insetGrouped
         )
+        configuration.showsSeparators = false
+        configuration.backgroundColor = .green
         
         let layout = UICollectionViewCompositionalLayout.list(
             using: configuration
@@ -45,5 +73,72 @@ class SimpleCollectionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.addSubview(collectionView)
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalTo(self.view.safeAreaLayoutGuide)
+        }
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        configureCell()
     }
+    
+    private func configureCell(){
+        //cellForItemAt 내부 코드
+        registration = UICollectionView.CellRegistration(handler: { cell, indexPath, itemIdentifier in
+            
+            var content = UIListContentConfiguration.valueCell()
+            
+            content.text = itemIdentifier.name
+            content.textProperties.color = .brown
+            
+            content.textProperties.font = .boldSystemFont(ofSize: 20)
+            
+            content.secondaryText = itemIdentifier.price.formatted() + "원"
+            content.secondaryTextProperties.color = .blue
+            
+            content.image = UIImage(systemName: "star")
+            content.imageProperties.tintColor = .orange
+            
+            cell.contentConfiguration = content
+            
+            var backgroundConfig = UIBackgroundConfiguration.listGroupedCell()
+            backgroundConfig.backgroundColor = .yellow
+            backgroundConfig.cornerRadius = 10
+            backgroundConfig.strokeWidth = 2.0
+            backgroundConfig.strokeColor = .red
+            
+            cell.backgroundConfiguration = backgroundConfig
+        })
+    }
+}
+
+extension SimpleCollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        list.count
+    }
+
+    /*
+     기존)
+     dequeueReusableCell
+     customCell + identifier + register
+     
+     New)
+     dequeueConfiguredReusableCell
+     systemCell +     x     + CellRegisteration
+     */
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueConfiguredReusableCell(
+            using: registration,
+            for: indexPath,
+            item: list[indexPath.item]
+        )
+    
+        return cell
+    }
+
+    
 }
