a3b7dc411de1268887700bad391089aee04caec4
rmfls0606 <rmfls0606@naver.com>
2025-02-27 09:47:30 +0900

    collectionView 생성 및 레이아웃 작성

--- RxSwiftStudy3/CompositionViewController.swift
+++ RxSwiftStudy3/CompositionViewController.swift
@@ -6,12 +6,40 @@
 //
 
 import UIKit
+import SnapKit
 
 class CompositionViewController: UIViewController {
-
+    
+    lazy var collectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: createLayout()
+    )
+    
+    func createLayout() -> UICollectionViewLayout{
+        
+        var configuration = UICollectionLayoutListConfiguration(
+            appearance: .insetGrouped
+        )
+        configuration.backgroundColor = .purple
+        configuration.showsSeparators = true
+//        configuration.leadingSwipeActionsConfigurationProvider
+        
+        let layout = UICollectionViewCompositionalLayout.list(using: configuration)
+        
+        return layout
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        configure()
+    }
+    
+    private func configure(){
+        self.view.backgroundColor = .white
+        self.view.addSubview(collectionView)
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalTo(self.view.safeAreaLayoutGuide)
+        }
     }
 }
