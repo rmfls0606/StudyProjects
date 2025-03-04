ad58a00dd95b955ce9fd87dc1b024461b347d957
rmfls0606 <rmfls0606@naver.com>
2025-03-04 16:42:17 +0900

    좋아요 누른 데이터를 데이터베이스에 저장하기위한 모델 생성

---
+++ NaverShopping/NaverShopping/Model/LikeTable.swift
@@ -0,0 +1,34 @@
+//
+//  LikeTable.swift
+//  NaverShopping
+//
+//  Created by 이상민 on 3/4/25.
+//
+
+import Foundation
+import RealmSwift
+
+class LikeTable: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var imageNamge: String
+    @Persisted var productName: String
+    @Persisted var productContent: String
+    @Persisted var price: String
+    
+    convenience init(
+        id: ObjectId,
+        imageNamge: String,
+        productName: String,
+        productContent: String,
+        price: String
+    ) {
+        self.init()
+        self.id = id
+        self.imageNamge = imageNamge
+        self.productName = productName
+        self.productContent = productContent
+        self.price = price
+    }
+}
+
+
