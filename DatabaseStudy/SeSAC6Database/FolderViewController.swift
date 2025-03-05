73d98db4f9f9c61af124a9087e2cf5458a3565a9
rmfls0606 <rmfls0606@naver.com>
2025-03-05 11:59:15 +0900

    관계형 모델 보기위한 VC생성

---
+++ DatabaseStudy/SeSAC6Database/FolderViewController.swift
@@ -0,0 +1,66 @@
+//
+//  FolderViewController.swift
+//  SeSAC6Database
+//
+//  Created by 이상민 on 3/5/25.
+//
+
+import UIKit
+import SnapKit
+import RealmSwift
+
+class FolderViewController: UIViewController {
+
+    let tableView = UITableView()
+    
+    var list: Results<Folder>! = nil
+    let repository: FolderRepositoryProtocol = FolderRepository()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureHierarchy()
+        configureView()
+        configureConstraints()
+        list = repository.fetchAllCase()
+        dump(list)
+    }
+    
+    private func configureHierarchy() {
+        view.addSubview(tableView)
+    }
+    
+    private func configureView() {
+        view.backgroundColor = .white
+        tableView.rowHeight = 130
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(ListTableViewCell.self, forCellReuseIdentifier: ListTableViewCell.id)
+    }
+    
+    private func configureConstraints() {
+         
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+}
+
+extension FolderViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        list.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: ListTableViewCell.id,
+            for: indexPath
+        ) as! ListTableViewCell
+        let data = list[indexPath.row]
+        cell.titleLabel.text = data.name
+        cell.subTitleLabel.text = "\(data.detail.count)개"
+        return cell
+    }
+
+    
+}
